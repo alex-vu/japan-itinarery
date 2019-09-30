755807fbf2470f9c2e1e63e1e4912c84a33f7a20
Alex <alex.vu6@outlook.com>
2019-09-30 17:41:35 -0400

    update itinerary day 1-4

--- src/pages/Page404/Page404.js
+++ src/pages/Page404/Page404.js
@@ -1,10 +1,12 @@
 import React from "react";
+import "./Page404.scss";
 
-class Page404 extends React.Component {
-  state = {};
-  render() {
-    return <div>Page404</div>;
-  }
-}
+const Page404 = () => {
+  return (
+    <div className="page404 h1 flex-center">
+      <h1 className="h1 dark-text">Opps! Page not found!</h1>
+    </div>
+  );
+};
 
 export default Page404;
--- src/tokyo.js
+++ src/tokyo.js
@@ -63,20 +63,50 @@ export default [
     fields: [
       {
         itinerary: {
-          schedule: "10:30 am",
+          schedule: "8:30 am",
           location: "Leave Hotel"
         }
       },
       {
         itinerary: {
-          schedule: "13:00 am",
-          location: "Tsukiji Fish Market"
+          schedule: "9:30 am",
+          location: "Meiji Shrine"
         }
       },
       {
         itinerary: {
-          schedule: "14:00 am",
-          location: "Team Lab"
+          schedule: "11:45 am",
+          location: "Lunch Time"
+        }
+      },
+      {
+        itinerary: {
+          schedule: "12:30 pm",
+          location: "Harajuku"
+        }
+      },
+      {
+        itinerary: {
+          schedule: "1:30 pm",
+          location: "Nonbei Yokocho"
+        }
+      },
+      {
+        itinerary: {
+          schedule: "2:30 pm",
+          location: "Hachiko Statue"
+        }
+      },
+      {
+        itinerary: {
+          schedule: "3:00 pm",
+          location: "Shibuya"
+        }
+      },
+      {
+        itinerary: {
+          schedule: "5:00-5:30 pm",
+          location: "Dinner"
         }
       }
     ]
@@ -86,20 +116,56 @@ export default [
     fields: [
       {
         itinerary: {
-          schedule: "10:30 am",
+          schedule: "8:30 am",
           location: "Leave Hotel"
         }
       },
       {
         itinerary: {
-          schedule: "13:00 am",
-          location: "Tsukiji Fish Market"
+          schedule: "9:00 am",
+          location: "Sensoji Temple"
         }
       },
       {
         itinerary: {
-          schedule: "14:00 am",
-          location: "Team Lab"
+          schedule: "10:00 am",
+          location: "Asakusa Shrine"
+        }
+      },
+      {
+        itinerary: {
+          schedule: "11:00 am",
+          location: "Nakamise Shopping Street"
+        }
+      },
+      {
+        itinerary: {
+          schedule: "12:00 pm",
+          location: "Asakusa Nishi Sando"
+        }
+      },
+      {
+        itinerary: {
+          schedule: "1:00 pm",
+          location: "Tokyo National Museum"
+        }
+      },
+      {
+        itinerary: {
+          schedule: "3:00 pm",
+          location: "Museum of Nature and Science"
+        }
+      },
+      {
+        itinerary: {
+          schedule: "5:30 pm",
+          location: "Dinner"
+        }
+      },
+      {
+        itinerary: {
+          schedule: "6:30 pm",
+          location: "Ueno"
         }
       }
     ]
@@ -109,20 +175,44 @@ export default [
     fields: [
       {
         itinerary: {
-          schedule: "10:30 am",
+          schedule: "8:30 am",
           location: "Leave Hotel"
         }
       },
       {
         itinerary: {
-          schedule: "13:00 am",
-          location: "Tsukiji Fish Market"
+          schedule: "9:00 am",
+          location: "Imperial Palace/Edo Castle"
         }
       },
       {
         itinerary: {
-          schedule: "14:00 am",
-          location: "Team Lab"
+          schedule: "10:00 am",
+          location: "Imperial Palace East Garden"
+        }
+      },
+      {
+        itinerary: {
+          schedule: "11:00 am",
+          location: "Lunch Time"
+        }
+      },
+      {
+        itinerary: {
+          schedule: "12:30 am",
+          location: "Akihabara"
+        }
+      },
+      {
+        itinerary: {
+          schedule: "2:00 am",
+          location: "Edo Tokyo Museum"
+        }
+      },
+      {
+        itinerary: {
+          schedule: "4:30 am",
+          location: "Tokyo Solomachi"
         }
       }
     ]
